d89ce9782fd47c187abc3fce3c7c8f42068ceef7
MANPREETSINGHJAGGI <wecoinvisors@gmail.com>
2025-09-25 14:15:50 +0530

    Updated  frontend/app/dashboard/stocks/page.tsx

--- frontend/app/dashboard/stocks/page.tsx
+++ frontend/app/dashboard/stocks/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from
+ "react";
 import StockTable from "@/components/StockTable";
 import Link from "next/link";
 
@@ -119,7 +120,6 @@ export default function StocksDashboard() {
         {!loading && !error && stocks.length > 0 && (
           <StockTable
             stocks={stocks}
-            loading={loading}
             watchlist={watchlist}
             setWatchlist={setWatchlist}
           />
